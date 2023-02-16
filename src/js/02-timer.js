662e9349c3421cd571096bccdb4420c21900cd77
alionash <112432218+alionash@users.noreply.github.com>
2023-02-16 22:38:45 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -10,7 +10,7 @@ const hoursEl = document.querySelector('span[data-hours]');
 const minutesEl = document.querySelector('span[data-minutes]');
 const secondsEl = document.querySelector('span[data-seconds]');
 
-startBtnEl.disabled = true;
+btnStart.disabled = true;
 
 let ms = null;
 let selectedDate = null;
@@ -23,5 +23,43 @@ const options = {
     onClose(selectedDates) {
         selectedDate = selectedDates[0];
         ms = selectedDate - options.defaultDate;
+
+        if(ms < 0) {
+            window.alert('Please choose a date in the future');
+        } else {
+            btnStart.disabled = false;
+        }
+        return;
     }
+}
+
+flatpickr(inputEl, options);
+
+btnStart.addEventListener('click', onStart);
+
+function onStart(){
+intervalId = setInterval(convertMs, 1000);
+}
+
+function convertMs(ms) {
+ms = selectedDate - Date.now();
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+
+    dayEl.textContent = addLeadingZero(Math.floor(ms / day));
+
+    hoursEl.textContent = addLeadingZero(Math.floor((ms % day) / hour));
+
+    minutesEl.textContent = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+
+    secondsEl.textContent = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+}
+
+function addLeadingZero(value){
+    return String(value).padStart(2, 0)
 }
